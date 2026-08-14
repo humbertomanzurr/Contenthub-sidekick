4370b6a287c973be9334cd2e9e04781f1bdc364a
humbertomanzurr <humberto@revolabsmedia.com>
2026-08-14 12:35:10 -0500

    Delete src/lib/supabase.js

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://shvbedzlxkqfvrsvarzl.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_EmPNZRHeDUNoDJrH150hoQ_6NoiA_zr'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
